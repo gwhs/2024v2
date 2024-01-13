0e991b3bffb5b613e77d05a4831e302b70dd99dd
Rob Blair <robtheblair@gmail.com>
2024-01-13 14:15:58 -0800

    add tests, one that fails

---
+++ src/test/java/frc/robot/RobotTest.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class RobotTest {
+
+    
+    @Test
+    void testRobot() {
+        Robot main = new Robot();
+        Assertions.assertNotNull(main, "robot must not be null");
+    }
+    @Test
+    void testMathFail() {
+        final int answer = 4;
+        Assertions.assertEquals(answer, 3 + 2, "math test");
+    }
+}
+
+

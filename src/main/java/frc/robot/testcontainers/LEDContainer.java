3d5f5881027f5e71b746118781088f0b92938afd
Morgan Jiang <morganjiang@Morgans-MacBook-Air.local>
2024-01-20 13:24:30 -0800

    made commands for different led colors and then binded them to xbox controller buttons

--- src/main/java/frc/robot/testcontainers/LEDContainer.java
+++ src/main/java/frc/robot/testcontainers/LEDContainer.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.BaseContainer;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.ledcommands.ChangeLEDToBlue;
+import frc.robot.commands.ledcommands.ChangeLEDToGreen;
+import frc.robot.commands.ledcommands.ChangeLEDToRed;
 import frc.robot.subsystems.LEDSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.event.BooleanEvent;
@@ -27,7 +29,8 @@ public class LEDContainer implements BaseContainer {
 
     private void configureBindings() {
         xboxController.x().onTrue(new ChangeLEDToBlue(led));
-        // onTrue(new ChangeLEDToBlue(ledSubsystem));
+        xboxController.y().onTrue(new ChangeLEDToRed(led));
+        xboxController.a().onTrue(new ChangeLEDToGreen(led));
     }
 
 }

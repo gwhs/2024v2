d9b173c26fd3c78724b3ec6450fe85f4f7b90555
Morgan Jiang <morganjiang@Morgans-MacBook-Air.local>
2024-01-20 12:41:22 -0800

    made setled command

--- src/main/java/frc/robot/testcontainers/LEDContainer.java
+++ src/main/java/frc/robot/testcontainers/LEDContainer.java
@@ -4,9 +4,15 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.BaseContainer;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.LEDSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 
 public class LEDContainer implements BaseContainer {
   
+    private final XboxController xboxController = new XboxController(0);
+    LEDSubsystem led = new LEDSubsystem(9);
+    
     // todo: add intake subsystem
     private final CommandXboxController m_driverController =
         new CommandXboxController(OperatorConstants.kDriverControllerPort);
@@ -16,10 +22,8 @@ public class LEDContainer implements BaseContainer {
     }
 
 
-    private void configureBindings() {
-        
-    }
+    private void configureBindings() {}
 
-    LEDSubsystem led = new LEDSubsystem();
-    
+    if ()
+
 }

fad0e4ea8f55402ebccd4502569b9330c32ab557
unknown <chvoong@s.sfusd.edu>
2024-01-27 14:11:06 -0800

    testing pose2d

---
+++ src/main/java/frc/robot/commands/driveToPose.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+
+public class driveToPose extends Command {
+  /** Creates a new driveToPose. */
+  private final SwerveSubsystem m_Subsystem;
+  public driveToPose(double x, double y, Rotation2d rotation, SwerveSubsystem swerveSubsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    double xPos = x;
+    double yPos = y;
+    Rotation2d rot = rotation;
+    m_Subsystem = swerveSubsystem;
+    addRequirements(swerveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_Subsystem.driveToPose();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/testcontainers/DriveContainer.java
+++ src/main/java/frc/robot/testcontainers/DriveContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.BaseContainer;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Robot;
+import frc.robot.commands.driveToPose;
 import frc.robot.commands.swervedrive.drivebase.AbsoluteDrive;
 import frc.robot.commands.swervedrive.drivebase.AbsoluteFieldDrive;
 import frc.robot.commands.swervedrive.drivebase.AbsoluteDriveAdv;
@@ -140,6 +141,7 @@ public class DriveContainer implements BaseContainer
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     driverXbox.start().onTrue(new InstantCommand(drivebase::zeroGyro));    
     driverXbox.x().onTrue(new InstantCommand(drivebase::addFakeVisionReading));
+    driverXbox.a().whileTrue(new driveToPose(0, 0, null, drivebase));
   }
 
   /**

206fd7880de155a291d6bf04e82070758a80f86d
unknown <wjunye72@gmail.com>
2024-02-05 18:22:00 -0800

    x&y cord for red speaker, and fix x&y blue cord that chris mess up

--- src/main/java/frc/robot/Util/UtilMath.java
+++ src/main/java/frc/robot/Util/UtilMath.java
@@ -8,8 +8,11 @@ import edu.wpi.first.math.geometry.Pose2d;
 
 /** Add your docs here. */
 public class UtilMath {
-    public static final double SPEAKER_X = 5.5479; //in meters
-    public static final double SPEAKER_Y = -0.0254; //in meters
+    public static final double SPEAKER_X = -0.0381; //in meters
+    public static final double SPEAKER_Y = 5.5479; //in meters
+    public static final double RED_SPEAKER_X = 16.5793; //in meters
+    public static final double RED_SPEAKER_Y = 5.5479; //in meters
+    
 
 
     public static double caclucateRotateTheta(Pose2d pose, double targetX, double targetY){

af2d19ea89d24c6993d24aa7e20c1082a9d704f1
Chvoong24 <89755355+Chvoong24@users.noreply.github.com>
2024-02-17 13:38:57 -0800

    added extention command

--- src/main/java/frc/robot/commands/ReactionArmCommands/Extend.java
+++ src/main/java/frc/robot/commands/ReactionArmCommands/Extend.java
@@ -8,7 +8,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ReactionSubsystem;
 
 public class Extend extends Command {
-  ReactionSubsystem m_Subsystem;
+  private final ReactionSubsystem m_Subsystem;
+  private double currPos; //position in rotation
+  private double endPos;
   /** Creates a new Extend. */
   public Extend(ReactionSubsystem subsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -19,24 +21,30 @@ public class Extend extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_Subsystem.spinForward();
+    currPos = m_Subsystem.getPos();
+    endPos = currPos + (63.5/360); //adds the current position to the final which is the end pos.
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    currPos = m_Subsystem.getPos();
+    m_Subsystem.spinForward();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
+    m_Subsystem.stop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if( (currPos > (endPos + 0.19027)) || (currPos < (endPos - 0.19027)))
+    {
+      return true;
+    }
     return false;
   }
 }
--- src/main/java/frc/robot/subsystems/ReactionSubsystem.java
+++ src/main/java/frc/robot/subsystems/ReactionSubsystem.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -16,6 +15,7 @@ public class ReactionSubsystem extends SubsystemBase {
   public ReactionSubsystem(int armID, String canbus) {
     m_reactionArm = new TalonFX(armID, canbus);
     TalonFXConfiguration configs = new TalonFXConfiguration();
+    
     configs.CurrentLimits.withSupplyCurrentLimit(Constants.ReactionConstants.currentLimit);
   }
 
@@ -35,5 +35,11 @@ public class ReactionSubsystem extends SubsystemBase {
   public void stop()
   {
     m_reactionArm.set(0);
+  }
+
+  public double getPos()
+  {
+    double posVal = m_reactionArm.getRotorPosition().getValue();
+    return posVal;
   }
 }

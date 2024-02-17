360adfc49a829510274352d04bf68f93624f0feb
Chvoong24 <89755355+Chvoong24@users.noreply.github.com>
2024-02-17 11:39:23 -0800

    reaction bar motor

---
+++ src/main/java/frc/robot/subsystems/ReactionSubsystem.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ReactionSubsystem extends SubsystemBase {
+  private TalonFX m_reactionArm;
+  /** Creates a new ReactionSubsystem. */
+  public ReactionSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}

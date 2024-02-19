25583df1b4a228231656744945b8a12ccbaee9f9
Chvoong24 <89755355+Chvoong24@users.noreply.github.com>
2024-02-19 13:05:02 -0800

    work in progress

--- src/main/java/frc/robot/GameRobotContainer.java
+++ src/main/java/frc/robot/GameRobotContainer.java
@@ -11,7 +11,12 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.IntakeCommands.IntakePassNoteToPizzaBox;
 import frc.robot.commands.IntakeCommands.IntakePickUpFromGround;
 import frc.robot.commands.IntakeCommands.SpinIntakePID;
+import frc.robot.commands.ledcommands.ChangeLEDColor;
+import frc.robot.commands.ledcommands.ChangeLEDToBlue;
+import frc.robot.commands.ledcommands.ChangeLEDToGreen;
+import frc.robot.commands.ledcommands.ChangeLEDToRed;
 import frc.robot.commands.swervedrive.drivebase.TeleopDrive;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -23,7 +28,7 @@ public class GameRobotContainer implements BaseContainer {
   
     private final SwerveSubsystem m_drivebase;
     private final IntakeSubsystem m_IntakeSubsystem;
-
+    private final ArmSubsystem m_ArmSubsystem;
 
     public String getDriveTrainName(){
         return "swerve/hajel_kraken";
@@ -34,6 +39,8 @@ public class GameRobotContainer implements BaseContainer {
         m_drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
                                                                          getDriveTrainName()));
         m_IntakeSubsystem = new IntakeSubsystem(Constants.IntakeConstants.INTAKE_LOWER_INTAKE_ID, Constants.IntakeConstants.INTAKE_SPIN_MOTOR_ID, 0, "rio");
+
+        m_ArmSubsystem = new ArmSubsystem(0, getDriveTrainName(), 0, getDriveTrainName(), 0, 0, 0)
 
         LEDSubsystem led = new LEDSubsystem(Constants.LEDConstants.ledPortNumber);
 
@@ -61,7 +68,15 @@ public class GameRobotContainer implements BaseContainer {
 
         driverController.a().onTrue(new IntakePickUpFromGround(m_IntakeSubsystem));
         driverController.b().onTrue(new IntakePassNoteToPizzaBox(m_IntakeSubsystem));
-        driverController.start().onTrue(new InstantCommand(m_drivebase::zeroGyro));    
+        driverController.start().onTrue(new InstantCommand(m_drivebase::zeroGyro));   
+        
+        //This should be a parallel command with other stuff
+       /* / driverController.x().onTrue(new ChangeLEDToBlue(led));//pressing x on the controller runs a
+        driverController.y().onTrue(new ChangeLEDToRed(led));
+        driverController.b().onTrue(new ChangeLEDToGreen(led));
+        driverController.a().onTrue(new ChangeLEDColor(led, 255, 0, 255));
+        driverController.rightBumper().onTrue(new ChangeLEDColor(led, 0, 0, 0));
+        */
     }
 
     public void setMotorBrake(boolean brake)

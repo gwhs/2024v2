41637bcadc8e47009284d7d023c83e3b63ff0d7d
CatonQ <caton868@gmail.com>
2024-02-22 16:50:47 -0800

    done?

--- src/main/java/frc/robot/Util/UtilMotor.java
+++ src/main/java/frc/robot/Util/UtilMotor.java
@@ -11,59 +11,33 @@ import com.ctre.phoenix6.hardware.TalonFX;
 /** Add your docs here. */
 public class UtilMotor {
 
-    public static void configMotor(TalonFX motor1, TalonFX motor2) {
+    public static void configMotor(TalonFX motor) {
+        configMotor(motor, 0.11, 0.5, 0.0001, 0.12, 8, 40);
+    }
+
+    public static void configMotor(TalonFX motor, double kP, double kI, double kD, double kV, int peakVoltage, int peakCurrent) {
         TalonFXConfiguration configs = new TalonFXConfiguration();
     /* Voltage-based velocity requires a feed forward to account for the back-emf of the motor */
-    configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 2V output
-    configs.Slot0.kI = 0.5; // An error of 1 rotation per second increases output by 0.5V every second
-    configs.Slot0.kD = 0.0001; // A change of 1 rotation per second squared results in 0.01 volts output
-    configs.Slot0.kV = 0.12; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second
+    configs.Slot0.kP = kP; // An error of 1 rotation per second results in 2V output
+    configs.Slot0.kI = kI; // An error of 1 rotation per second increases output by 0.5V every second
+    configs.Slot0.kD = kD; // A change of 1 rotation per second squared results in 0.01 volts output
+    configs.Slot0.kV = kV; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second
     // Peak output of 8 volts
-    configs.Voltage.PeakForwardVoltage = 8;
-    configs.Voltage.PeakReverseVoltage = -8;
+    configs.Voltage.PeakForwardVoltage = peakVoltage;
+    configs.Voltage.PeakReverseVoltage = -peakVoltage;
 
     // Peak output of 40 amps
-    configs.TorqueCurrent.PeakForwardTorqueCurrent = 40;
-    configs.TorqueCurrent.PeakReverseTorqueCurrent = -40;
+    configs.TorqueCurrent.PeakForwardTorqueCurrent = peakCurrent;
+    configs.TorqueCurrent.PeakReverseTorqueCurrent = -peakCurrent;
 
     /* Retry config apply up to 5 times, report if failure */
-    StatusCode leftMotorStatus = StatusCode.StatusCodeNotInitialized;
-    StatusCode rightMotorStatus = StatusCode.StatusCodeNotInitialized;
+    StatusCode motorStatus = StatusCode.StatusCodeNotInitialized;
     for (int i = 0; i < 5; ++i) {
-      leftMotorStatus = motor1.getConfigurator().apply(configs);
-      rightMotorStatus = motor2.getConfigurator().apply(configs);
-      if (leftMotorStatus.isOK() && rightMotorStatus.isOK()) break;
+      motorStatus = motor.getConfigurator().apply(configs);
+      if (motorStatus.isOK()) break;
     }
-    if(!leftMotorStatus.isOK() || !rightMotorStatus.isOK()) {
-      System.out.println("Could not apply configs, error code: " + leftMotorStatus.toString());
-    }
-    }
-
-    public static void configMotor(TalonFX motor1, TalonFX motor2, boolean pizzaboxcheck) {
-        TalonFXConfiguration configs = new TalonFXConfiguration();
-    /* Voltage-based velocity requires a feed forward to account for the back-emf of the motor */
-    configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 2V output
-    configs.Slot0.kI = 0.5; // An error of 1 rotation per second increases output by 0.5V every second
-    configs.Slot0.kD = 0.0001; // A change of 1 rotation per second squared results in 0.01 volts output
-    configs.Slot0.kV = 0.12; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second
-    // Peak output of 8 volts
-    configs.Voltage.PeakForwardVoltage = 8;
-    configs.Voltage.PeakReverseVoltage = -8;
-
-    // Peak output of 40 amps
-    configs.TorqueCurrent.PeakForwardTorqueCurrent = 40;
-    configs.TorqueCurrent.PeakReverseTorqueCurrent = -40;
-
-    /* Retry config apply up to 5 times, report if failure */
-    StatusCode leftMotorStatus = StatusCode.StatusCodeNotInitialized;
-    StatusCode rightMotorStatus = StatusCode.StatusCodeNotInitialized;
-    for (int i = 0; i < 5; ++i) {
-      leftMotorStatus = motor1.getConfigurator().apply(configs);
-      rightMotorStatus = motor2.getConfigurator().apply(configs);
-      if (leftMotorStatus.isOK() && rightMotorStatus.isOK()) break;
-    }
-    if(!leftMotorStatus.isOK() || !rightMotorStatus.isOK()) {
-      System.out.println("Could not apply configs, error code: " + leftMotorStatus.toString());
+    if(!motorStatus.isOK()) {
+      System.out.println("Could not apply configs, error code: " + motorStatus.toString());
     }
     }
 }

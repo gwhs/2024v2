e778b1bcfe87664333a536532934cd24b8ae346c
Chvoong24 <89755355+Chvoong24@users.noreply.github.com>
2024-02-22 16:57:33 -0800

    shuffleboard

--- src/main/java/frc/robot/commands/driveCommands/rotateinPlace.java
+++ src/main/java/frc/robot/commands/driveCommands/rotateinPlace.java
@@ -8,6 +8,7 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -55,6 +56,7 @@ public class rotateinPlace extends Command {
 
     angleRate = PID.calculate(currTheta);
     m_Subsystem.drive(pose, angleRate, true);
+    SmartDashboard.putNumber("Error Rate", angleRate);
     }
     
   

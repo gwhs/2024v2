dc8a4146b2d67ded99e4c24b2894dc5c5724d9e5
Alvin G. <alguo1@s.sfusd.edu>
2024-02-26 17:44:08 -0800

    armSubsystem is cleaned up

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -17,6 +17,7 @@ import com.ctre.phoenix6.StatusCode;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
+import frc.robot.Util.UtilMotor;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
@@ -69,53 +70,11 @@ public class ArmSubsystem extends ProfiledPIDSubsystem {
       m_arm = new TalonFX(armId, armCanbus);
       m_encoder = new DutyCycleEncoder(channel1);
       armFeedForward = new ArmFeedforward(Arm.KSVOLTS, Arm.KGVOLTS, 0, 0);
-      
-      StaticBrake brake = new StaticBrake();
-      
-      
-
-      
+          
       targetArmAngle(encoderGetAngle());
       enable();
 
-      
-
-      // Needed? PIDController ArmPID = new PIDController(0, 0, 0);
-
-
-      TalonFXConfiguration configs = new TalonFXConfiguration();
-      m_arm.setNeutralMode(NeutralModeValue.Brake);
-      m_arm.setControl(new NeutralOut());
-      m_arm.setNeutralMode(NeutralModeValue.Brake);
-
-      StaticBrake brake = new StaticBrake();
-      m_arm.setControl(brake);
-
-      /* Voltage-based velocity requires a feed forward to account for the back-emf of the motor */
-      configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 2V output
-      configs.Slot0.kI = 0; // An error of 1 rotation per second increases output by 0.5V every second
-      configs.Slot0.kD = 0; // A change of 1 rotation per second squared results in 0.01 volts output
-      configs.Slot0.kV = 0.12; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second
-      // Peak output of 8 volts
-      configs.Voltage.PeakForwardVoltage = 15;
-      configs.Voltage.PeakReverseVoltage = -15;
-  
-      // Peak output of 40 amps
-      configs.TorqueCurrent.PeakForwardTorqueCurrent = 50;
-      configs.TorqueCurrent.PeakReverseTorqueCurrent = -50;
-  
-      /* Retry config apply up to 5 times, report if failure */
-      StatusCode motorStatus = StatusCode.StatusCodeNotInitialized;
-      for (int i = 0; i < 5; ++i) {
-        motorStatus = m_arm.getConfigurator().apply(configs);
-        if (motorStatus .isOK()) break;
-      }
-      if(!motorStatus.isOK()) {
-        System.out.println("Could not apply configs, error code: " + motorStatus.toString());
-      }
-
-       m_arm.setNeutralMode(NeutralModeValue.Brake);
-       m_arm.setControl(brake);
+    UtilMotor.configMotor(m_arm, .11, 0, 0, .12, 15, 50, true);      
 
     Shuffleboard.getTab("Arm").addDouble("Encoder Angle", ()->encoderGetAngle()).withWidget(BuiltInWidgets.kGraph)
     .withSize(3,3)

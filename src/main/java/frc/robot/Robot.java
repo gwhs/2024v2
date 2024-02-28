02f2b9b7aef27e1ea7a0a407cec67fcd3092d1ef
Christopher <89755355+Chvoong24@users.noreply.github.com>
2024-02-27 20:21:53 -0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.ReactionSubsystem;
 import frc.robot.testcontainers.ArmContainer;
 import frc.robot.testcontainers.ClimbContainer;
 import frc.robot.testcontainers.DriveContainer;
@@ -33,7 +32,6 @@ public class Robot extends TimedRobot {
   public static final String LED = "LED";
   public static final String REACTION = "REACTION";
 
-  public static ReactionSubsystem m_ReactionSubsystem;
 
   // change this to match the subsystem container you want to use, or GAME for complete robot
   public static final String container = GAME;

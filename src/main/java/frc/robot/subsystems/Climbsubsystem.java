82b6c7465b37e6ed0e9548ebc85b2382be814463
Alvin G. <alguo1@s.sfusd.edu>
2024-03-06 15:23:19 -0800

    stop climb

--- src/main/java/frc/robot/subsystems/Climbsubsystem.java
+++ src/main/java/frc/robot/subsystems/Climbsubsystem.java
@@ -35,6 +35,7 @@ public class Climbsubsystem extends SubsystemBase {
   private double rightGoalDistance = 198.4;
 
   private boolean checkForUp = false;
+  private boolean emergencyStop = false;
 
   DigitalInput bottomLeft = new DigitalInput(ClimbConstants.BOT_LEFT_LIMIT_ID);
   DigitalInput bottomRight = new DigitalInput(ClimbConstants.BOT_RIGHT_LIMIT_ID);
@@ -127,8 +128,14 @@ public class Climbsubsystem extends SubsystemBase {
   } 
 
   public void stopClimbMotorInCaseOfEmergencySoThisWillStopTheClimbNoMatterIfItIsGoingUpOrDown() {
-    leftPIDcontroller.setGoal(getPositionLeft());
-    rightPIDcontroller.setGoal(getPositionRight());
+    if(!emergencyStop) {
+      leftPIDcontroller.setGoal(getPositionLeft());
+      rightPIDcontroller.setGoal(getPositionRight());
+      emergencyStop = true;
+    }
+    else{
+      emergencyStop = false;
+    }
   }
 
   public void upMotor() {
@@ -161,9 +168,10 @@ public class Climbsubsystem extends SubsystemBase {
     if (!checkForUp && getBotRightLimit()) {
       rightPIDvalue = 0;
     }
-
-
-    setSpeed(-leftPIDvalue, rightPIDvalue);
+    
+    if (!emergencyStop) {
+      setSpeed(-leftPIDvalue, rightPIDvalue);
+    }
     
 
   }

62ab5beb7ea0dd22ac811cf14da37ef0bef8be9c
chvoong24 <chvoong@s.sfusd.edu>
2024-03-09 12:56:18 -0800

    disable arm

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -52,7 +52,7 @@ public class ArmSubsystem extends ProfiledPIDSubsystem {
   private TalonFX m_arm;
   private DutyCycleEncoder m_encoder;
   private ArmFeedforward armFeedForward;
-  public boolean emergencyStop = false;
+  public boolean emergencyStop = true;
 
   public ArmSubsystem(int armId, String armCanbus, int channel1)
   {

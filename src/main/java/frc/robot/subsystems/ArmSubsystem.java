3c9952f5b5b9f3e7ffa4ce518814e6cdf9f028e1
Alvin G. <alguo1@s.sfusd.edu>
2024-03-10 12:04:50 -0700

    arm encoder check

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -119,8 +119,7 @@ public class ArmSubsystem extends ProfiledPIDSubsystem {
   }
 
   public boolean checkEncoderAngleForClimb() {
-    return true;
-    //return (encoderGetAngle() >= 125 && encoderGetAngle() <= 180);
+    return (encoderGetAngle() >= 125 && encoderGetAngle() <= 180);
   }
 
   @Override

878d470d2a2bfc1c805cc386c252a0e3a5306ab3
CatonQ <caton868@gmail.com>
2024-04-05 16:28:02 -0700

    auto

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -128,7 +128,7 @@ public class SwerveSubsystem extends SubsystemBase
                                          // Max module speed, in m/s
                                          swerveDrive.swerveDriveConfiguration.getDriveBaseRadiusMeters(),
                                          // Drive base radius in meters. Distance from robot center to furthest module.
-                                         new ReplanningConfig(true, true)
+                                         new ReplanningConfig(true, true,.25,.25)
                                          // Default path replanning config. See the API for the options here
         ),
         () -> {

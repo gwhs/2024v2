f23295dcdd391ec45fddc4f7bf214137c09694be
chvoong24 <chvoong@s.sfusd.edu>
2024-04-05 19:26:45 -0700

    fix facespeaker

--- src/main/java/frc/robot/commands/driveCommands/FaceSpeaker.java
+++ src/main/java/frc/robot/commands/driveCommands/FaceSpeaker.java
@@ -18,7 +18,7 @@ public class FaceSpeaker extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    drive.faceSpeaker = !drive.faceSpeaker;
+    drive.faceSpeaker = true;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -27,7 +27,9 @@ public class FaceSpeaker extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drive.faceSpeaker = false;
+  }
 
   // Returns true when the command should end.
   @Override

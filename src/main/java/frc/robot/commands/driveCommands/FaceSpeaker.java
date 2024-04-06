cdf6819772110d6974ea9bf4169c33e1baa839a9
CatonQ <caton868@gmail.com>
2024-04-05 19:30:10 -0700

    speaker

--- src/main/java/frc/robot/commands/driveCommands/FaceSpeaker.java
+++ src/main/java/frc/robot/commands/driveCommands/FaceSpeaker.java
@@ -34,6 +34,6 @@ public class FaceSpeaker extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return false;
   }
 }

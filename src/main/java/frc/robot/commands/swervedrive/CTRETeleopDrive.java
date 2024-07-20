e390da4f2840613d4d442ee6f39048769d39b83e
CatonQ <caton868@gmail.com>
2024-07-19 17:56:58 -0700

    turning keybings

--- src/main/java/frc/robot/commands/swervedrive/CTRETeleopDrive.java
+++ src/main/java/frc/robot/commands/swervedrive/CTRETeleopDrive.java
@@ -7,6 +7,7 @@ package frc.robot.commands.swervedrive;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -14,6 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
+import frc.robot.Constants.OperatorConstants;
 import frc.robot.Util.UtilMath;
 import frc.robot.subsystems.swervedrive.CommandSwerveDrivetrain;
 import frc.robot.subsystems.swervedrive.TunerConstants;
@@ -61,7 +63,8 @@ public class CTRETeleopDrive extends Command {
   public void execute() {
     double xVelocity = -driverController.getLeftY() * MaxSpeed;
     double yVelocity = -driverController.getLeftX() * MaxSpeed;
-    double angularVelocity = -driverController.getRightX() * MaxAngularRate;
+    //double angularVelocity = -driverController.getRightX() * MaxAngularRate;
+    double angularVelocity = MathUtil.applyDeadband(driverController.getLeftTriggerAxis(), OperatorConstants.ROTATION_DEADBAND) - MathUtil.applyDeadband(driverController.getLeftTriggerAxis(), OperatorConstants.ROTATION_DEADBAND);
 
     if(DriverStation.getAlliance().isPresent() && 
        DriverStation.getAlliance().get() == DriverStation.Alliance.Red)

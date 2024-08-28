8d1b1acba1bbd7647fd44c2e2707c74745f19d16
R1shy <kadlecekemma@gmail.com>
2024-08-28 16:05:03 -0700

    deleted and replaced the pizza box subssystem

---
+++ src/main/java/frc/robot/subsystems/PizzaBoxSubsystem/PizzaBoxConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.PizzaBoxSubsystem;
+
+
+
+
+public class PizzaBoxConstants {}
---
+++ src/main/java/frc/robot/subsystems/PizzaBoxSubsystem/PizzaBoxMain.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.PizzaBoxSubsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PizzaBoxMain extends SubsystemBase {
+  
+  public PizzaBoxMain() {}
+
+  @Override
+  public void periodic() {
+    
+  }
+}

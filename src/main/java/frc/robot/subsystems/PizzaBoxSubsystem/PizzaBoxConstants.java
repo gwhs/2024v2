a6d03a7bb2ad39314389ec67c9d3bc6092a60588
R1shy <kadlecekemma@gmail.com>
2024-08-28 16:40:10 -0700

    setup the new pizza box subsystem

--- src/main/java/frc/robot/subsystems/PizzaBoxSubsystem/PizzaBoxConstants.java
+++ src/main/java/frc/robot/subsystems/PizzaBoxSubsystem/PizzaBoxConstants.java
@@ -4,5 +4,12 @@ package frc.robot.subsystems.PizzaBoxSubsystem;
 
 
 public class PizzaBoxConstants {
-    public final String Brain_cells_wasted = "a lot";
+    //important stuff start
+    public final static String PizzaBoxCAN = "rio";
+    public final static int PizzaBoxID = 23;
+    public final static int servoPWD = 0;
+    public final static int flapPWD = 1;
+    // important stuff end
+
+    public final static double stop = .00;
 }
--- src/main/java/frc/robot/subsystems/PizzaBoxSubsystem/PizzaBoxSubsystem.java
+++ src/main/java/frc/robot/subsystems/PizzaBoxSubsystem/PizzaBoxSubsystem.java
@@ -1,10 +1,27 @@
 package frc.robot.subsystems.PizzaBoxSubsystem;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PizzaBoxSubsystem extends SubsystemBase {
   
-  public PizzaBoxSubsystem() {}
+  private TalonFX m_PizzaBoxMotor = new TalonFX(PizzaBoxConstants.PizzaBoxID,PizzaBoxConstants.PizzaBoxCAN);
+  private Servo PBservo;
+  private Servo PBFlapServo;
+  public PizzaBoxSubsystem() {
+    m_PizzaBoxMotor = new TalonFX(PizzaBoxConstants.PizzaBoxID,PizzaBoxConstants.PizzaBoxCAN);
+    PBservo = new Servo(PizzaBoxConstants.servoPWD);
+    PBFlapServo = new Servo(PizzaBoxConstants.flapPWD);
+
+  }
+
+  public void reset(Servo s, Servo s2, TalonFX f) {
+    s.set(PizzaBoxConstants.stop);
+    s2.set(PizzaBoxConstants.stop);
+    f.set(PizzaBoxConstants.stop);
+  }
 
   @Override
   public void periodic() {

700a60add4d0f928c8b2d9a7616c5d1a0ec85af7
R1shy <kadlecekemma@gmail.com>
2024-08-28 16:53:26 -0700

    added a reset command to the pizzabox

--- src/main/java/frc/robot/subsystems/PizzaBoxSubsystem/PizzaBoxSubsystem.java
+++ src/main/java/frc/robot/subsystems/PizzaBoxSubsystem/PizzaBoxSubsystem.java
@@ -1,10 +1,10 @@
 package frc.robot.subsystems.PizzaBoxSubsystem;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+// COMMANDS NEED TO BE RUN WITH LAMBDAS
 public class PizzaBoxSubsystem extends SubsystemBase {
   
   private TalonFX m_PizzaBoxMotor = new TalonFX(PizzaBoxConstants.PizzaBoxID,PizzaBoxConstants.PizzaBoxCAN);
@@ -17,14 +17,26 @@ public class PizzaBoxSubsystem extends SubsystemBase {
 
   }
 
-  public void reset(Servo s, Servo s2, TalonFX f) {
-    s.set(PizzaBoxConstants.stop);
-    s2.set(PizzaBoxConstants.stop);
-    f.set(PizzaBoxConstants.stop);
-  }
+  
 
   @Override
   public void periodic() {
     
+  }
+
+  public String reset(Servo s, Servo s2, TalonFX f) {
+    s.set(PizzaBoxConstants.stop);
+    s2.set(PizzaBoxConstants.stop);
+    f.set(PizzaBoxConstants.stop);
+  
+    return "RESETING ANGLE (NO TOUCHY THE PIZZABOX)";
+  }
+
+  public Command reset_command(Servo s, Servo s2, TalonFX f) {
+    s.set(PizzaBoxConstants.stop);
+    s2.set(PizzaBoxConstants.stop);
+    f.set(PizzaBoxConstants.stop);
+
+    return this.runOnce(() -> reset(PBservo,PBFlapServo,m_PizzaBoxMotor));
   }
 }

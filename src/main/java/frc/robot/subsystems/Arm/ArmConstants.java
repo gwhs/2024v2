4738fc08867295a7410d58ade81a4ec4f1c2861a
Morgan Jiang <morganjiang9768@Gmail.com>
2024-09-04 16:46:31 -0700

    made method to get encoder angle' git push

--- src/main/java/frc/robot/subsystems/Arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmConstants.java
@@ -15,4 +15,6 @@ public class ArmConstants {
     public static final double ARM_kD = 0;
     public static final double ARM_VEL = 230 * Math.PI / 180;
     public static final double ARM_ACC = 360 * Math.PI / 180;
+    public static final int ARM_ROTATION_TO_DEGREES = 360;
+    public static final double ARM_ENCODER_OFFSET = 18.6;
 }
--- src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
@@ -7,21 +7,36 @@ package frc.robot.subsystems.Arm;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 
 public class ArmSubsystem extends SubsystemBase {
-  /** Creates a new ArmSubsystem. */
+  
+  private TalonFX m_armMotor = new TalonFX(ArmConstants.ARM_armMotor_ID, ArmConstants.ARM_armMotor_CAN);
+  private DutyCycleEncoder m_armEncoder = m_armEncoder = new DutyCycleEncoder(ArmConstants.ARM_armEncoder_CHANNEL);
+  private Constraints constraints = new Constraints(ArmConstants.ARM_VEL, ArmConstants.ARM_ACC);
+  private ProfiledPIDController pidController = new ProfiledPIDController(ArmConstants.ARM_kP, ArmConstants.ARM_kI, ArmConstants.ARM_kP, constraints);
+
   public ArmSubsystem() {
-    TalonFX m_armMotor = new TalonFX(ArmConstants.ARM_armMotor_ID, ArmConstants.ARM_armMotor_CAN);
-    DutyCycleEncoder m_armEncoder = new DutyCycleEncoder(ArmConstants.ARM_armEncoder_CHANNEL);
-    Constraints constraints = new Constraints(ArmConstants.ARM_VEL, ArmConstants.ARM_ACC);
-    ProfiledPIDController pid = new ProfiledPIDController(ArmConstants.ARM_kP, ArmConstants.ARM_kI, ArmConstants.ARM_kP, constraints);
+  }
+  
+  public double getArmEncoderAngle() {
+    return m_armEncoder.getAbsolutePosition()*ArmConstants.ARM_ROTATION_TO_DEGREES - ArmConstants.ARM_ENCODER_OFFSET;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    double pidOutput = pidController.calculate(getArmEncoderAngle());
+
+    pidOutput = MathUtil.clamp(pidOutput, -1, 1);
+    m_armMotor.set(pidOutput);
   }
+
+  // public Command spinArm(double targetAngle) {
+  //   return this.runOnce((-> ))
+  // }
 }

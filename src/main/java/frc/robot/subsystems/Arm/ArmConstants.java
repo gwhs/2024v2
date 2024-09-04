852e43f88b8cba2249a61161a73c3ee18555111b
Morgan Jiang <morganjiang9768@Gmail.com>
2024-09-04 16:23:04 -0700

    created motor, encoder, pid and constraints

--- src/main/java/frc/robot/subsystems/Arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmConstants.java
@@ -5,4 +5,14 @@
 package frc.robot.subsystems.Arm;
 
 /** Add your docs here. */
-public class ArmConstants {}
+public class ArmConstants {
+
+    public static final int ARM_armMotor_ID = 18;
+    public static final String ARM_armMotor_CAN = "rio";
+    public static final int ARM_armEncoder_CHANNEL = 0;
+    public static final double ARM_kP = 8;
+    public static final double ARM_kI = 0;
+    public static final double ARM_kD = 0;
+    public static final double ARM_VEL = 230 * Math.PI / 180;
+    public static final double ARM_ACC = 360 * Math.PI / 180;
+}
--- src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
@@ -4,11 +4,21 @@
 
 package frc.robot.subsystems.Arm;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+  public ArmSubsystem() {
+    TalonFX m_armMotor = new TalonFX(ArmConstants.ARM_armMotor_ID, ArmConstants.ARM_armMotor_CAN);
+    DutyCycleEncoder m_armEncoder = new DutyCycleEncoder(ArmConstants.ARM_armEncoder_CHANNEL);
+    Constraints constraints = new Constraints(ArmConstants.ARM_VEL, ArmConstants.ARM_ACC);
+    ProfiledPIDController pid = new ProfiledPIDController(ArmConstants.ARM_kP, ArmConstants.ARM_kI, ArmConstants.ARM_kP, constraints);
+  }
 
   @Override
   public void periodic() {

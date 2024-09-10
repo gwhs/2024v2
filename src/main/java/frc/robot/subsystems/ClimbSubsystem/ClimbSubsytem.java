2844662d3141c615097526c4fce7a54f2ff3a4d1
Caden <Cachen15@s.sfusd.edu>
2024-09-09 17:44:20 -0700

    Added Shuffle board stuff

--- src/main/java/frc/robot/subsystems/ClimbSubsystem/ClimbSubsytem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem/ClimbSubsytem.java
@@ -4,9 +4,15 @@
 
 package frc.robot.subsystems.ClimbSubsystem;
 
+import java.util.Map;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -23,40 +29,37 @@ public class ClimbSubsytem extends SubsystemBase {
     private ProfiledPIDController leftpidController = new ProfiledPIDController(ClimbConstants.CLIMB_PID_KP, ClimbConstants.CLIMB_PID_KI, ClimbConstants.CLIMB_PID_KD, constraints);
     private ProfiledPIDController rightpidController = new ProfiledPIDController(ClimbConstants.CLIMB_PID_KP, ClimbConstants.CLIMB_PID_KI, ClimbConstants.CLIMB_PID_KD, constraints);
 
+    public ClimbSubsytem() {
+        ShuffleboardTab tab = Shuffleboard.getTab("Testing");
+        ShuffleboardLayout climbCommandsLayout = tab.getLayout("TestingCommands", BuiltInLayouts.kList)
+            .withSize(2, 2)
+            .withProperties(Map.of("Label position", "HIDDEN"));
 
-@Override
-
-public void periodic() {
-    double rightpidOutput = rightpidController.calculate(m_rightClimbMotor.getPosition().getValue());
-    double leftpidOutput = leftpidController.calculate(m_leftClimbMotor.getPosition().getValue());
-
-    m_leftClimbMotor.set(leftpidOutput);
-    m_rightClimbMotor.set(rightpidOutput);
-}
-
-public Command motorUp() {
-    return this.runOnce(()   -> {
-        leftpidController.setGoal(ClimbConstants.LEFT_UP_POSITION);
-        rightpidController.setGoal(ClimbConstants.RIGHT_UP_POSITION);
-    });
-
-
-
-
+        climbCommandsLayout.add(motorUp());
+        climbCommandsLayout.add(motorDown());
 
 }
 
-public Command motorDown() {
-    return this.runOnce(()   -> {
-        leftpidController.setGoal(ClimbConstants.LEFT_DOWN_POSITION);
-        rightpidController.setGoal(ClimbConstants.RIGHT_DOWN_POSITION);
-    });
+    @Override
+    public void periodic() {
+        double rightpidOutput = rightpidController.calculate(m_rightClimbMotor.getPosition().getValue());
+        double leftpidOutput = leftpidController.calculate(m_leftClimbMotor.getPosition().getValue());
 
-
-
-
+        m_leftClimbMotor.set(leftpidOutput);
+        m_rightClimbMotor.set(rightpidOutput);
     }
 
-    
+    public Command motorUp() {
+        return this.runOnce(()   -> {
+            leftpidController.setGoal(ClimbConstants.LEFT_UP_POSITION);
+            rightpidController.setGoal(ClimbConstants.RIGHT_UP_POSITION);
+        });
+    }
 
+    public Command motorDown() {
+        return this.runOnce(()   -> {
+            leftpidController.setGoal(ClimbConstants.LEFT_DOWN_POSITION);
+            rightpidController.setGoal(ClimbConstants.RIGHT_DOWN_POSITION);
+        });
+    }
 }

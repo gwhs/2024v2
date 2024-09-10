603c4a785e84ae2e0ca1cc7dcd08a990b93dba2f
Isaac Ng <isaacngkh@gmail.com>
2024-09-09 20:53:18 -0700

    uncommented out intake command

--- src/main/java/frc/robot/GameRobotContainer.java
+++ src/main/java/frc/robot/GameRobotContainer.java
@@ -151,7 +151,7 @@ public class GameRobotContainer implements BaseContainer {
       
       driverController.y().onTrue(new ScoreInSpeakerUnderHand(m_PizzaBoxSubsystem, m_ArmSubsystem));
       driverController.a().onTrue(new ScoreInAmp(m_PizzaBoxSubsystem, m_ArmSubsystem, drive)); 
-      //driverController.b().onTrue(new PickUpFromGroundAndPassToPizzaBox(m_PizzaBoxSubsystem,m_ArmSubsystem, m_IntakeSubsystem, 10));
+      driverController.b().onTrue(new PickUpFromGroundAndPassToPizzaBox(m_PizzaBoxSubsystem,m_ArmSubsystem, m_IntakeSubsystem, 10));
       driverController.x().whileTrue(new DecreaseSpeed(drive));
 
       driverController.rightStick().onTrue(new ScoreInSpeakerHigh(m_PizzaBoxSubsystem, m_ArmSubsystem));

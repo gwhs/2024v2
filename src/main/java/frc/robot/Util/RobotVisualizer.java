6cdfc915b551b416a70553ff077305160bbd6801
wilsomc <wilsonchan115@gmail.com>
2024-09-23 16:45:36 -0700

    test

--- src/main/java/frc/robot/Util/RobotVisualizer.java
+++ src/main/java/frc/robot/Util/RobotVisualizer.java
@@ -6,6 +6,8 @@ package frc.robot.Util;
 
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -16,17 +18,17 @@ public class RobotVisualizer {
   private final IntakeSubsystem intakeSubsystem;
   private final Mechanism2d panel = new Mechanism2d(ROBOT_LENGTH, ROBOT_LENGTH);
 
+  private final MechanismLigament2d m_arm;
+  private final MechanismLigament2d m_wrist;
+
   // Robot Constants
   public static final double ROBOT_LENGTH = Units.inchesToMeters(28);
 
-
-  
   // TO DO: use MechanismRoot2d and MechanismLigament2d to form stick figures that represent the arm
-
-
-
+  m_arm = root.append(new MechanismLigament2d("elevator", kElevatorMinimumLength, 90));
+  
   // TO DO: use MechanismRoot2d and MechanismLigament2d to form stick figures that represent the intake
-
+  m_wrist = m_elevator.append(new MechanismLigament2d("wrist", 0.5, 90, 6, new Color8Bit(Color.kPurple)));
 
 
   public RobotVisualizer(ArmSubsystem armSubsystem, IntakeSubsystem intakeSubsystem) {
@@ -44,11 +46,11 @@ public class RobotVisualizer {
     double armAngle = SmartDashboard.getNumber("Robot Visualizer/Pretend Arm Angle", 90);
 
     //TO DO: Update arm angles in stick figures; 90 degrees is straight down. 180 degrees is perpendicular to floor and above intake
+    m_arm.setAngle();
 
 
-
-    //TO DO: Update intake arm angles in stick figures; 0 degree is deployed position, 92 degrees is retracted position
-
+    //TO DO: Update intake arm angles in stick figures; 0 degree is deployed n, 92 degrees is retracted position
+    m_wrist.setAngle();
 
 
     SmartDashboard.putData("Robot Visualizer/panel", panel);

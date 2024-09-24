d32e5a2d8c756c69240a39ef749da38aa09da24b
Zhyang4916 <2006zhide@gmail.com>
2024-09-24 14:14:34 -0700

    robot visualizer code mechanism 2d

--- src/main/java/frc/robot/Util/RobotVisualizer.java
+++ src/main/java/frc/robot/Util/RobotVisualizer.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 
 /** Add your docs here. */
 public class RobotVisualizer {
@@ -18,17 +20,17 @@ public class RobotVisualizer {
   private final IntakeSubsystem intakeSubsystem;
   private final Mechanism2d panel = new Mechanism2d(ROBOT_LENGTH, ROBOT_LENGTH);
 
-  private final MechanismLigament2d m_arm;
-  private final MechanismLigament2d m_wrist;
 
   // Robot Constants
   public static final double ROBOT_LENGTH = Units.inchesToMeters(28);
 
   // TO DO: use MechanismRoot2d and MechanismLigament2d to form stick figures that represent the arm
-  m_arm = root.append(new MechanismLigament2d("elevator", kElevatorMinimumLength, 90));
+
+  MechanismRoot2d root = panel.getRoot("armSim", 0.7, 0); 
+  MechanismLigament2d m_arm = root.append(new MechanismLigament2d("arm", 0.5, 90));
   
   // TO DO: use MechanismRoot2d and MechanismLigament2d to form stick figures that represent the intake
-  m_wrist = m_elevator.append(new MechanismLigament2d("wrist", 0.5, 90, 6, new Color8Bit(Color.kPurple)));
+  MechanismLigament2d m_spinner = m_arm.append(new MechanismLigament2d("spinner", 0.5, 90, 6, new Color8Bit(Color.kPurple)));
 
 
   public RobotVisualizer(ArmSubsystem armSubsystem, IntakeSubsystem intakeSubsystem) {
@@ -46,11 +48,11 @@ public class RobotVisualizer {
     double armAngle = SmartDashboard.getNumber("Robot Visualizer/Pretend Arm Angle", 90);
 
     //TO DO: Update arm angles in stick figures; 90 degrees is straight down. 180 degrees is perpendicular to floor and above intake
-    m_arm.setAngle();
+    m_arm.setAngle(intakeArmAngle);
 
 
     //TO DO: Update intake arm angles in stick figures; 0 degree is deployed n, 92 degrees is retracted position
-    m_wrist.setAngle();
+    m_spinner.setAngle(armAngle);
 
 
     SmartDashboard.putData("Robot Visualizer/panel", panel);

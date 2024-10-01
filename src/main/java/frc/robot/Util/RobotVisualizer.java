38712d8c0040288db333b43f56a5704327ccf853
Zhyang4916 <2006zhide@gmail.com>
2024-09-30 17:21:49 -0700

    code on robot visualize

--- src/main/java/frc/robot/Util/RobotVisualizer.java
+++ src/main/java/frc/robot/Util/RobotVisualizer.java
@@ -26,33 +26,33 @@ public class RobotVisualizer {
 
   // TO DO: use MechanismRoot2d and MechanismLigament2d to form stick figures that represent the arm
 
-  MechanismRoot2d root = panel.getRoot("armSim", 0.3, 0.1); 
-  MechanismLigament2d m_arm = root.append(new MechanismLigament2d("arm", 0.3, 45));
+  MechanismRoot2d root = panel.getRoot("armSim", 0.4, 0.1); 
+  MechanismLigament2d m_arm = root.append(new MechanismLigament2d("arm", 0.3, 90));
   
   // TO DO: use MechanismRoot2d and MechanismLigament2d to form stick figures that represent the intake
-  MechanismLigament2d m_spinner = m_arm.append(new MechanismLigament2d("spinner", 0.5, 90, 6, new Color8Bit(Color.kPurple)));
+  MechanismLigament2d m_spinner = m_arm.append(new MechanismLigament2d("spinner", 0.2, 270, 12, new Color8Bit(Color.kPurple)));
 
 
   public RobotVisualizer(ArmSubsystem armSubsystem, IntakeSubsystem intakeSubsystem) {
     this.armSubsystem = armSubsystem;
     this.intakeSubsystem = intakeSubsystem;
 
-    SmartDashboard.putNumber("Robot Visualizer/Pretend Intake Angle", 30);
+    SmartDashboard.putNumber("Robot Visualizer/Pretend Intake Angle", 90);
     SmartDashboard.putNumber("Robot Visualizer/Pretend Arm Angle", 90);
   }
 
   public void update() {
     //TO DO: When intake subsystem is finished, replace this method to return actual intake arm angle
-    double intakeArmAngle = SmartDashboard.getNumber("Robot Visualizer/Pretend Intake Angle", 0);
+    double intakeArmAngle = SmartDashboard.getNumber("Robot Visualizer/Pretend Intake Angle", 90);
     //TO DO: When arm subsystem is finished, replace this method to return actual arm angle
     double armAngle = SmartDashboard.getNumber("Robot Visualizer/Pretend Arm Angle", 90);
 
     //TO DO: Update arm angles in stick figures; 90 degrees is straight down. 180 degrees is perpendicular to floor and above intake
-    m_arm.setAngle(intakeArmAngle);
+    m_arm.setAngle(armAngle);
 
 
     //TO DO: Update intake arm angles in stick figures; 0 degree is deployed n, 92 degrees is retracted position
-    m_spinner.setAngle(armAngle);
+    m_spinner.setAngle(-intakeArmAngle-90);
 
 
     SmartDashboard.putData("Robot Visualizer/panel", panel);

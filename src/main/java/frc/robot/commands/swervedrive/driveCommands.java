df9217106bb4f1b04d0e5ef4d294b3f012dfcc62
chvoong24 <chvoong@s.sfusd.edu>
2024-10-23 16:33:57 -0700

    dw

--- src/main/java/frc/robot/commands/swervedrive/driveCommands.java
+++ src/main/java/frc/robot/commands/swervedrive/driveCommands.java
@@ -1,5 +1,14 @@
 package frc.robot.commands.swervedrive;
 
+import edu.wpi.first.wpilibj2.command.Command;
+
+public boolean slowDrive = false;
+
 public class driveCommands {
+
+    public Command slowDrive() {
+        return driv
+
+    }
     
 }

29aed5cd6e6bf2d4a1ed8b4b2b9b1533f2c1071f
Morgan Jiang <morganjiang9768@Gmail.com>
2024-11-04 16:28:47 -0800

    done with motion magic but the logging isn't showing up on advantage scope

--- src/main/java/frc/robot/subsystems/ClimbSubsystem/ClimbSubsytem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem/ClimbSubsytem.java
@@ -26,12 +26,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 /** Add your docs here. */
 public class ClimbSubsytem extends SubsystemBase {
   private ClimbIO climbIO;
-  private Constraints constraints = new Constraints(ClimbConstants.MAX_VELOCITY, ClimbConstants.MAX_ACCELERATION);
-
-  private ProfiledPIDController leftpidController = new ProfiledPIDController(ClimbConstants.CLIMB_PID_KP,
-      ClimbConstants.CLIMB_PID_KI, ClimbConstants.CLIMB_PID_KD, constraints);
-  private ProfiledPIDController rightpidController = new ProfiledPIDController(ClimbConstants.CLIMB_PID_KP,
-      ClimbConstants.CLIMB_PID_KI, ClimbConstants.CLIMB_PID_KD, constraints);
 
   public ClimbSubsytem() {
 
@@ -43,9 +37,6 @@ public class ClimbSubsytem extends SubsystemBase {
       
     }
 
-    leftpidController.setGoal(climbIO.getLeftMotorPosition());
-    rightpidController.setGoal(climbIO.getRightMotorPosition());
-
     ShuffleboardTab tab = Shuffleboard.getTab("Testing");
     ShuffleboardLayout climbCommandsLayout = tab.getLayout("TestingCommands", BuiltInLayouts.kList)
         .withSize(2, 2)
@@ -54,30 +45,12 @@ public class ClimbSubsytem extends SubsystemBase {
     climbCommandsLayout.add(motorUp());
     climbCommandsLayout.add(motorDown());
 
-    SmartDashboard.putData("LeftPIDController", leftpidController);
-    SmartDashboard.putData("RightPIDController", rightpidController);
-
   }
 
   @Override
-  public void periodic() {
-    double rightpidOutput = rightpidController.calculate(climbIO.getRightMotorPosition());
-    double leftpidOutput = leftpidController.calculate(climbIO.getLeftMotorPosition());
-
-
-    rightpidOutput = MathUtil.clamp(rightpidOutput, -1, 1);
-    leftpidOutput = MathUtil.clamp(leftpidOutput, -1, 1);
-
-    climbIO.setLeftMotorSpeed(leftpidOutput);
-    climbIO.setRightMotorSpeed(rightpidOutput);
-
-    
+  public void periodic() {  
     climbIO.update();
 
-    NetworkTableInstance.getDefault().getEntry("Climb/Left PID Output").setNumber(leftpidOutput);
-    NetworkTableInstance.getDefault().getEntry("Climb/Right PID Output").setNumber(rightpidOutput);
-    NetworkTableInstance.getDefault().getEntry("Climb/Left PID Goal").setNumber(leftpidController.getGoal().position);
-    NetworkTableInstance.getDefault().getEntry("Climb/Right PID Goal").setNumber(rightpidController.getGoal().position);
     NetworkTableInstance.getDefault().getEntry("Climb/Left motor Position").setNumber(climbIO.getLeftMotorPosition());
     NetworkTableInstance.getDefault().getEntry("Climb/Right motor Position").setNumber(climbIO.getRightMotorPosition());
 

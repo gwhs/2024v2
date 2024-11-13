fda6f3eeb18a3bad81743095b6176bea26f8e29e
Caden740 <118308692+Caden740@users.noreply.github.com>
2024-11-13 15:23:20 -0800

    Disable intake motors when encoder is disconnected (#92)
    * did method for if encoder is disconnected stop intake
    
    * i did some stuff to the methods in the intake substystem. I added my encoder method thing
    
    * i make log for intake
    
    * changed name
    
    * remove tracking for simgui
    
    ---------
    
    Co-authored-by: chvoong24 <chvoong@s.sfusd.edu>
    Co-authored-by: Isaac Ng <isaacngkh@gmail.com>

--- src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -65,7 +65,12 @@ public class IntakeSubsystem extends SubsystemBase {
 
     pidOutput = MathUtil.clamp(pidOutput, -1, 1);
 
-    intakeIO.setArmSpeed(pidOutput);
+    if (intakeIO.isEncoderConnected()) {
+      intakeIO.setArmSpeed(pidOutput);
+    }
+    else {
+      intakeIO.setArmSpeed(0);
+    }
 
     intakeIO.update();
 
@@ -80,6 +85,7 @@ public class IntakeSubsystem extends SubsystemBase {
 
     NetworkTableInstance.getDefault().getEntry("Intake/Intake Deployed").setBoolean(isDeployed.getAsBoolean());
     NetworkTableInstance.getDefault().getEntry("Intake/Note Sensor").setBoolean(intakeIO.getNoteSensor());
+    NetworkTableInstance.getDefault().getEntry("Intake/Encoder Connected").setBoolean(intakeIO.isEncoderConnected());
   }
 
   public double getArmAngle() {
@@ -89,7 +95,6 @@ public class IntakeSubsystem extends SubsystemBase {
   public double getSpinSpeed() {
     return intakeIO.getSpinSpeed();
   }
-
   public Command deployIntake() {
     return this.runOnce(() -> {
       pidController.setGoal(IntakeConstants.DOWN_POSITION);
@@ -98,6 +103,7 @@ public class IntakeSubsystem extends SubsystemBase {
         .andThen(
             Commands.waitUntil(noteTriggered),
             retractIntake())
+        .onlyIf(() -> intakeIO.isEncoderConnected())
         .withName("Intake: deploy intake");
   }
 
@@ -107,18 +113,21 @@ public class IntakeSubsystem extends SubsystemBase {
       intakeIO.setSpinSpeed(0);
     })
         .andThen(Commands.waitUntil(() -> pidController.atGoal()))
+        .onlyIf(() -> intakeIO.isEncoderConnected())
         .withName("Intake: retract intake");
   }
 
   public Command intakeNote() {
     return this.runOnce(() -> {
       intakeIO.setSpinSpeed(1);
-    });
+    })
+        .onlyIf(() -> intakeIO.isEncoderConnected());
   }
 
   public Command stopIntake() {
     return this.runOnce(() -> {
       intakeIO.setSpinSpeed(0);
-    });
+    })
+        .onlyIf(() -> intakeIO.isEncoderConnected());
   }
 }

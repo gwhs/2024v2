33c386fc2bc4a5badba154c5564fff1df00f9a7c
Caden740 <118308692+Caden740@users.noreply.github.com>
2024-11-13 15:26:47 -0800

    Making sure encoder is connected before running arm commands (#93)
    * init
    
    * made it so that if encoder isn't connected, the arm won't work
    
    * i make log
    
    * i change name
    
    * remove tracking for simgui
    
    * I change the arm encoder code in periodic
    
    ---------
    
    Co-authored-by: chvoong24 <chvoong@s.sfusd.edu>
    Co-authored-by: Isaac Ng <isaacngkh@gmail.com>

--- src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
@@ -59,11 +59,18 @@ public class ArmSubsystem extends SubsystemBase {
     double speed = pidOutput + armFeedforwardOutput;
 
     pidOutput = MathUtil.clamp(pidOutput, -1, 1);
-    armIO.setArmSpeed(speed);
+    if (armIO.isEncoderConnected()) {
+      armIO.setArmSpeed(speed);
+    }
+    else {
+      armIO.setArmSpeed(0);
+    }
+
     NetworkTableInstance.getDefault().getEntry("/Arm/ArmAngle").setNumber(armIO.getArmEncoderAngle());
     NetworkTableInstance.getDefault().getEntry("/Arm/pidOutput").setNumber(pidOutput);
     NetworkTableInstance.getDefault().getEntry("/Arm/armGoal")
         .setNumber(Units.radiansToDegrees(pidController.getGoal().position));
+    NetworkTableInstance.getDefault().getEntry("/Arm/EncoderConnected").setBoolean(armIO.isEncoderConnected());
     NetworkTableInstance.getDefault().getEntry("/Arm/FeedforwardOutput").setNumber(armFeedforwardOutput);
     NetworkTableInstance.getDefault().getEntry("/Arm/armSpeed").setNumber(speed);
     armIO.update();
@@ -72,6 +79,7 @@ public class ArmSubsystem extends SubsystemBase {
   public Command spinArm(double targetAngle) {
     return this.runOnce(() -> {
       pidController.setGoal(Units.degreesToRadians(targetAngle));
-    }).andThen(Commands.waitUntil(() -> pidController.atGoal()));
+    }).andThen(Commands.waitUntil(() -> pidController.atGoal()))
+    .onlyIf(() -> armIO.isEncoderConnected());
   }
 }

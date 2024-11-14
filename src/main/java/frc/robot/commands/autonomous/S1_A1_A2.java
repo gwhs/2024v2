71d35895943522ac01d7376c90ad6c57112585af
Caden <Cachen15@s.sfusd.edu>
2024-11-13 16:52:29 -0800

    I made auto path, still has bugs

---
+++ src/main/java/frc/robot/commands/autonomous/S1_A1_A2.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.GameRobotContainer;
+import frc.robot.subsystems.Arm.ArmSubsystem;
+import frc.robot.subsystems.Intake.IntakeSubsystem;
+import frc.robot.subsystems.PizzaBox.PizzaBoxSubsystem;
+
+public class S1_A1_A2 extends PathPlannerAuto {
+  public S1_A1_A2(GameRobotContainer robotContainer, ArmSubsystem armSubsystem, IntakeSubsystem intakeSubsystem,
+      PizzaBoxSubsystem pizzaBoxSubsystem) {
+    super(Commands.run(() -> {
+    }));
+
+    /* All your code should go inside this try-catch block */
+    try {
+      /* TODO: Load all paths needed */
+      PathPlannerPath S1A1 = PathPlannerPath.fromPathFile("S1-A1");
+      PathPlannerPath A1A2 = PathPlannerPath.fromPathFile("A1-A2");
+
+      /* TODO: Get starting position of starting path */
+      Pose2d startingPose = new Pose2d(
+          S1A1.getPoint(0).position,
+          S1A1.getIdealStartingState().rotation());
+
+      /* TODO: When autonomous begins */
+      isRunning().onTrue(
+          Commands.sequence(
+              AutoBuilder.resetOdom(startingPose)),
+              robotContainer.scoreSpeaker(160),
+              AutoBuilder.followPath(S1A1).alongWith(robotContainer.deployIntake()));
+      
+      event("atA1").and(intakeSubsystem.noteTriggered).onTrue(
+        Commands.sequence(
+          AutoBuilder.followPath("S1A1").alongwith(robotContainer.retractIntakePassToPB()),
+          robotContainer.scoreSpeaker(236),
+          AutoBuilder.followPath("A1A2").alongwith(robotContainer.deployIntake();)
+        )
+      )
+      /* TODO: Other triggers */
+
+    } catch (Exception e) {
+      DriverStation.reportError("Path Not Found: " + e.getMessage(), e.getStackTrace());
+    }
+  }
+}

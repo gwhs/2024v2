ad3ca0a4ab35691b1b776acd60026e81d9b4c3b9
Zhyang4916 <2006zhide@gmail.com>
2024-11-18 17:23:10 -0800

    b_S3_C5_C4

--- src/main/java/frc/robot/GameRobotContainer.java
+++ src/main/java/frc/robot/GameRobotContainer.java
@@ -116,7 +116,8 @@ public class GameRobotContainer implements BaseContainer {
     autoChooser.addOption("S2-Leave", new S2Leave(this, m_ArmSubsystem, m_IntakeSubsystem, m_PizzaBoxSubsystem));
     autoChooser.addOption("S3-C5", new S3_C5(this, m_ArmSubsystem, m_IntakeSubsystem, m_PizzaBoxSubsystem));
     autoChooser.addOption("S3-C5-C4", new S3_C5_C4(this, m_ArmSubsystem, m_IntakeSubsystem, m_PizzaBoxSubsystem));
-    
+    autoChooser.addOption("b_S3_C5_C4", new b_S3_C5_C4(this, m_ArmSubsystem, m_IntakeSubsystem, m_PizzaBoxSubsystem));
+
     //TODO: add more autonomous routines
 
     SmartDashboard.putData("autonomous", autoChooser);

b4349d06940c695f8c2d0fc64eaa6c42f8871c1a
Caden Chen <Cachen15@s.sfusd.edu>
2024-12-04 15:59:45 -0800

    fix speedy arm

--- src/main/java/frc/robot/subsystems/PizzaBox/PizzaBoxSubsystem.java
+++ src/main/java/frc/robot/subsystems/PizzaBox/PizzaBoxSubsystem.java
@@ -65,7 +65,7 @@ public class PizzaBoxSubsystem extends SubsystemBase {
 
   public Command speedyArm_Command(DoubleSupplier f) {
     return this.run(() -> {
-      if(f.getAsDouble() <= 100 || f.getAsDouble() >= 270) {
+      if(f.getAsDouble() >= 100 || f.getAsDouble() <= 270) {
         pizzaBoxIO.setMotor(1);
       }
       else {
